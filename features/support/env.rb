4c775dba59a80a1c3b57d1d5f692434cfe6e0e80
yeshwant-singh <yeshwant@browserstack.com>
2015-01-21 18:03:21 +0530

    Update env.rb

--- features/support/env.rb
+++ features/support/env.rb
@@ -8,10 +8,10 @@ if ENV['REMOTE'] == "true"
 
   url = "http://#{ENV['HUB_USERNAME']}:#{ENV['AUTH_KEY']}@#{ENV['HUB_NAME']}.browserstack.com:#{ENV['HUB_PORT']}/wd/hub"
   capabilities = Selenium::WebDriver::Remote::Capabilities.new
-  capabilities['os'] = ENV['OS']
-  capabilities['os_version'] = ENV['OS_VERSION']
-  capabilities['browser'] = ENV['BROWSER']
-  capabilities['browser_version'] = ENV['BROWSER_VERSION']
+  #capabilities['os'] = ENV['OS']
+  #capabilities['os_version'] = ENV['OS_VERSION']
+  #capabilities['browser'] = ENV['BROWSER']
+  #capabilities['browser_version'] = ENV['BROWSER_VERSION']
   capabilities['build'] = ENV['BUILD'] || "Testing Parallel"
   capabilities['browserstack.debug'] = 'true'
 
